3eb1539a9ca5d4c050e398506df0ca82201b0acc
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-22 09:04:23 +0300

    Generate tweet scafold

---
+++ db/migrate/20221222060356_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tweets do |t|
+      t.text :body
+      t.integer :likes
+      t.integer :retweets
+
+      t.timestamps
+    end
+  end
+end
